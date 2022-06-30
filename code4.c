15ea8fb9b5b503d4d73b3bc24cd2b38411a2b549
Nandini Agarwal <98901861+neophilic-coder@users.noreply.github.com>
2022-06-30 18:08:50 +0530

    Create code4.c

---
+++ code4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>    
+#include<stdlib.h>
+
+int main()
+{
+	int pid = fork();  
+	if (pid>0){
+		printf("Process ID: %d\n",getpid());
+		sleep(60);}
+	else if(pid==0){
+		printf("Hello from Child:\n");
+		printf("ID for Parent:%d\n",getppid()); 
+		printf("ID for Child: %d\n",getpid());
+		printf("Zombie process created successfully");
+		exit(0);}
+	else
+		printf("Fork command was not successful");
+	return 0;
+}
